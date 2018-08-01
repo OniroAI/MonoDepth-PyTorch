24288b9d18bf90cef38a6188a8a67dce8d96bad2
Alex <alexander.skorikov@gmail.com>
2018-08-01 11:06:20 +0200

    feat: Use PyTorch's grid_sample instead of handmade bilinear sampler

--- bilinear_sampler_pytorch.py
+++ bilinear_sampler_pytorch.py
@@ -1,72 +1,17 @@
-from __future__ import absolute_import, division, print_function
 import torch
-from torch.nn.functional import pad
+import torch.nn.functional as F
 
-def apply_disparity(input_images, x_offset, wrap_mode='border', tensor_type = 'torch.cuda.FloatTensor'):
-    num_batch, num_channels, height, width = input_images.size()
 
-    # Handle both texture border types
-    edge_size = 0
-    if wrap_mode == 'border':
-        edge_size = 1
-        # Pad last and second-to-last dimensions by 1 from both sides
-        input_images = pad(input_images, (1, 1, 1, 1))
-    elif wrap_mode == 'edge':
-        edge_size = 0
-    else:
-        return None
+def apply_disparity(img, disp, tensor_type='torch.cuda.FloatTensor'):
+    batch_size, _, height, width = img.size()
+    
+    # Original coordinates of pixels
+    x_base = torch.linspace(-1, 1, width).repeat(batch_size, height, 1).type(tensor_type)
+    y_base = torch.linspace(-1, 1, height).repeat(batch_size, width, 1).transpose(1, 2).type(tensor_type)
 
-    # Put channels to slowest dimension and flatten batch with respect to others
-    input_images = input_images.permute(1, 0, 2, 3).contiguous()
-    im_flat = input_images.view(num_channels, -1)
-
-    # Create meshgrid for pixel indicies (PyTorch doesn't have dedicated
-    # meshgrid function)
-    x = torch.linspace(0, width - 1, width).repeat(height, 1).type(tensor_type)
-    y = torch.linspace(0, height - 1, height).repeat(width, 1).transpose(0, 1).type(tensor_type)
-    # Take padding into account
-    x = x + edge_size
-    y = y + edge_size
-    # Flatten and repeat for each image in the batch
-    x = x.view(-1).repeat(1, num_batch)
-    y = y.view(-1).repeat(1, num_batch)
-
-    # Now we want to sample pixels with indicies shifted by disparity in X direction
-    # For that we convert disparity from % to pixels and add to X indicies
-    x = x + x_offset.contiguous().view(-1) * width
-    # Make sure we don't go outside of image
-    x = torch.clamp(x, 0.0, width - 1 + 2 * edge_size)
-    # Round disparity to sample from integer-valued pixel grid
-    y0 = torch.floor(y)
-    # In X direction round both down and up to apply linear interpolation
-    # between them later
-    x0 = torch.floor(x)
-    x1 = x0 + 1
-    # After rounding up we might go outside the image boundaries again
-    x1 = x1.clamp(max=(width - 1 + 2 * edge_size))
-
-    # Calculate indices to draw from flattened version of image batch
-    dim2 = (width + 2 * edge_size)
-    dim1 = (width + 2 * edge_size) * (height + 2 * edge_size)
-    # Set offsets for each image in the batch
-    base = dim1 * torch.arange(num_batch).type(tensor_type)
-    base = base.view(-1, 1).repeat(1, height * width).view(-1)
-    # One pixel shift in Y  direction equals dim2 shift in flattened array
-    base_y0 = base + y0 * dim2
-    # Add two versions of shifts in X direction separately
-    idx_l = base_y0 + x0
-    idx_r = base_y0 + x1
-
-    # Sample pixels from images
-    pix_l = im_flat.gather(1, idx_l.repeat(num_channels, 1).long())
-    pix_r = im_flat.gather(1, idx_r.repeat(num_channels, 1).long())
-
-    # Apply linear interpolation to account for fractional offsets
-    weight_l = x1 - x
-    weight_r = x - x0
-    output = weight_l * pix_l + weight_r * pix_r
-
-    # Reshape back into image batch and permute back to (N,C,H,W) shape
-    output = output.view(num_channels, num_batch, height, width).permute(1,0,2,3)
+    # Apply shift in X direction
+    x_shifts = disp[:,0,:,:] # Disparity is passed in NCHW format with 1 channel
+    flow_field = torch.stack((x_base + x_shifts, y_base), dim=3)
+    output = F.grid_sample(img, flow_field, mode='bilinear', padding_mode='zeros')
 
     return output
