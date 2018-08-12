362d0a570c51d798d7de0a607f902a6f33b28ac6
voeykovroman <voeykoroman@gmail.com>
2018-08-13 01:56:14 +0300

    refactor: Replace nn.functional.sigmoid with nn.Sigmoid to avoid warning messages

--- models_resnet.py
+++ models_resnet.py
@@ -124,13 +124,14 @@ class get_disp(nn.Module):
         super(get_disp, self).__init__()
         self.conv1 = nn.Conv2d(num_in_layers, 2, kernel_size=3, stride=1)
         self.normalize = nn.BatchNorm2d(2)
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x):
         p = 1
         p2d = (p, p, p, p)
         x = self.conv1(F.pad(x, p2d))
         x = self.normalize(x)
-        return 0.3 * F.sigmoid(x)
+        return 0.3 * self.sigmoid(x)
 
 
 class resnet50_md(nn.Module):
