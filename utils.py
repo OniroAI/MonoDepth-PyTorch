f9e24c2d9ce87a33654ef989f0cbb2e40863d875
voeykovroman <voeykovroman@gmail.com>
2018-09-13 02:34:42 +0300

    feat: Add model with useful functions

---
+++ utils.py
@@ -0,0 +1,47 @@
+import torch
+import collections
+import os
+from torch.utils.data import DataLoader, ConcatDataset
+
+import models_resnet
+from data_loader import KittiLoader
+from transforms import image_transforms
+
+def to_device(input, device):
+    if torch.is_tensor(input):
+        return input.to(device=device)
+    elif isinstance(input, str):
+        return input
+    elif isinstance(input, collections.Mapping):
+        return {k: to_device(sample, device=device) for k, sample in input.items()}
+    elif isinstance(input, collections.Sequence):
+        return [to_device(sample, device=device) for sample in input]
+    else:
+        raise TypeError(f"Input must contain tensor, dict or list, found {type(input)}")
+
+
+def get_model(model):
+    if model == 'resnet50_md':
+        out_model = models_resnet.resnet50_md(3)
+    elif model == 'resnet18_md':
+        out_model = models_resnet.resnet18_md(3)
+    return out_model
+
+
+def prepare_dataloader(data_directory, mode, augment_parameters, do_augmentation, batch_size, size):
+    data_dirs = os.listdir(data_directory)
+    data_transform = image_transforms(
+        mode=mode,
+        augment_parameters=augment_parameters,
+        do_augmentation=do_augmentation,
+        size = size)
+    datasets = [KittiLoader(os.path.join(data_directory,
+                                               data_dir), mode,
+                                  transform=data_transform) for data_dir in
+                      data_dirs]
+    dataset = ConcatDataset(datasets)
+    n_img = len(dataset)
+    print('Use a dataset with', n_img, 'images')
+    loader = DataLoader(dataset, batch_size= batch_size,
+                        shuffle=True)
+    return n_img, loader
